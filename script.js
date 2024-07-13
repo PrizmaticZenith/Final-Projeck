631b7bafdfab8e4219e5263a6432417e5f10518b
PrizmaticZenith <hanif3januari@gmail.com>
2024-07-13 20:27:14 +0700

    hampir habis

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function toggleDetail (e) {
     const detail = $(item).children(".about-exp-item-detail")
     $(detail).slideToggle()
 }
+
+function onFormSubmit (e) {
+    e.preventDefault()
+    const email = $("#inp-email")
+    const subject = $("#inp-subject")
+    const Message = $("#inp-Message")
+
+    if (!$(email).val()) {
+        alert("Harus memasukkan Email")
+    } else if (!$(subject).val()) {
+        alert("Harus ada Subject")
+    } else if (!$(Message).val()) {
+        alert("Harus ada Message")
+    } else {
+        alert("Form sudah di Kirim/Submit")
+        $(email).val("")
+        $(subject).val("")
+        $(Message).val("")
+    }
+}
